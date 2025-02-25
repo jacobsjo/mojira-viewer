b23964ce46f538848fc46652b7d3b1e5afa0777a
Jochen Jacobs <JochenJacobs33@gmail.com>
2025-02-25 03:00:40 +0100

    pages workflow and fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 import path from 'path'
-import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    svgLoader(),
     VueI18nPlugin({
       include: [path.resolve(__dirname, './locales/**')]
     }),
   ],
   build: {
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        404: path.resolve(__dirname, '404.html')
+      }
+    }
   }
 })
