98549d55c783355e6572160a8d13ccc02a0ee6fa
Jochen Jacobs <JochenJacobs33@gmail.com>
2025-03-01 00:10:25 +0100

    fix default saved issues

--- src/stores/SavedIssuesStore.ts
+++ src/stores/SavedIssuesStore.ts
@@ -2,7 +2,7 @@ import { useStorage } from "@vueuse/core";
 import { defineStore } from "pinia";
 
 export const useSavedIssuesStore = defineStore('savedIssues', () => {
-    const savedIssues = useStorage<Set<string>>('saved-issues', new Set(''))
+    const savedIssues = useStorage<Set<string>>('saved-issues', new Set<string>())
 
     return { savedIssues }
 })
