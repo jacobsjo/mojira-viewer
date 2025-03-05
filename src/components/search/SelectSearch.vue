141304aefaff8b43819132f8f18be03f4c74bbe2
Jochen Jacobs <JochenJacobs33@gmail.com>
2025-03-05 15:51:46 +0100

    allow searching for projects by key
    resolves #2

--- src/components/search/SelectSearch.vue
+++ src/components/search/SelectSearch.vue
@@ -8,8 +8,18 @@ const meta = FieldMetadata.Select[query.value.field]
 </script>
 
 <template>
-    <v-select :placeholder="$t(`search.${query.field}.placeholder`,$t(`field.${query.field}.label`)+': Any')" multiple :options="meta.options" :taggable="meta.any ?? false"
-        v-model="query.selected" :reduce="(s: any) => (typeof s === 'string') ? s : s.key" />
+    <v-select multiple 
+        :placeholder="$t(`search.${query.field}.placeholder`,$t(`field.${query.field}.label`)+': Any')"
+        :options="meta.options"
+        :taggable="meta.any ?? false"
+        v-model="query.selected"
+        :reduce="(s: any) => (typeof s === 'string') ? s : s.key"
+        :filter="(options: (string | {'key': string, 'label': string})[] , search: string) => options.filter((s: any) => (
+            (typeof s === 'string')
+            ? s.toLowerCase().includes(search.toLowerCase())
+            : s.label.toLowerCase().includes(search.toLowerCase()) || s.key.toLowerCase().startsWith(search.toLowerCase())
+        ))"
+        />
 </template>
 
 <style scoped>
